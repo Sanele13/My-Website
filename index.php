ec2ddd59623383b14605ccef6eaf272f246fdd56
Sanele13 <aynstyn@gmail.com>
2017-11-11 14:34:49 +0200

    create menu icon
    I have created the menu icon. I will now check if it displays well.

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php 
+echo "
+<html>
+	<head>
+		<link rel = 'stylesheet' type = 'text/css' href = 'mystyle.css' />
+		<title>Sanele Mpangalala</title>
+		<script src='https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js'></script>
+	</head>
+	<body>
+		<div class = 'menu-icon'>
+			<div class = 'bar-ting'></div>
+			<div class = 'bar-ting'></div>
+			<div class = 'bar-ting'></div>
+		</div>
+	<body>
+</html>
+";
+
+?>
