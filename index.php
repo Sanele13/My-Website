a98a9bcd282ec566ca495f0acb3772e3b9e506d0
Sanele13 <aynstyn@gmail.com>
2017-11-11 15:14:31 +0200

    create menu icon
    I created the menu item. It respods to a click. At the moment the animation is on zero! So I gotta jQuery this ting to make it slide to the left, instead of just showing up!

--- index.php
+++ index.php
@@ -5,12 +5,24 @@ echo "
 		<link rel = 'stylesheet' type = 'text/css' href = 'mystyle.css' />
 		<title>Sanele Mpangalala</title>
 		<script src='https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js'></script>
+		<script>
+			function showMenu(){
+				document.getElementById('menu').style.display='inline-block';
+			}
+		</script>
 	</head>
 	<body>
-		<div class = 'menu-icon'>
-			<div class = 'bar-ting'></div>
-			<div class = 'bar-ting'></div>
-			<div class = 'bar-ting'></div>
+		<div class = 'container'>
+			<div class = 'menu-icon2' id = 'menu'>
+				<div class = 'bar-ting2'></div>
+				<div class = 'bar-ting2'></div>
+				<div class = 'bar-ting2'></div>
+			</div>
+			<div class = 'menu-icon'  onclick = 'showMenu()'>
+				<div class = 'bar-ting'></div>
+				<div class = 'bar-ting'></div>
+				<div class = 'bar-ting'></div>
+			</div>
 		</div>
 	<body>
 </html>
