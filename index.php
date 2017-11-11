faf878006d8b23481d0408511828c0b10924c824
Sanele13 <aynstyn@gmail.com>
2017-11-11 17:01:32 +0200

    create menu icon
    Things are looking okay. I jQueried this ting!

--- index.php
+++ index.php
@@ -1,24 +1,35 @@
 <?php 
 echo "
 <html>
+<html>
 	<head>
 		<link rel = 'stylesheet' type = 'text/css' href = 'mystyle.css' />
 		<title>Sanele Mpangalala</title>
 		<script src='https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js'></script>
 		<script>
-			function showMenu(){
-				document.getElementById('menu').style.display='inline-block';
-			}
+			
+			$(document).ready(function(){
+				var clicked=0;
+				$('.menu-icon').click(function(){
+					var leftVal = '10px';
+					
+					if(clicked===0){
+						$('.menu-icon2').animate({left:'250px'}, "slow");clicked=1;
+					}
+					else{$('.menu-icon2').animate({left:leftVal}, "slow");clicked=0;}
+					
+				});
+			});
 		</script>
 	</head>
 	<body>
 		<div class = 'container'>
-			<div class = 'menu-icon2' id = 'menu'>
+			<div class = 'menu-icon2' >
 				<div class = 'bar-ting2'></div>
 				<div class = 'bar-ting2'></div>
 				<div class = 'bar-ting2'></div>
 			</div>
-			<div class = 'menu-icon'  onclick = 'showMenu()'>
+			<div class = 'menu-icon'  id = 'menu'>
 				<div class = 'bar-ting'></div>
 				<div class = 'bar-ting'></div>
 				<div class = 'bar-ting'></div>
