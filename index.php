8bff4276bf50a085ab5baa0854be822bda3cff00
Sanele13 <aynstyn@gmail.com>
2017-11-12 22:12:29 +0200

    Done!
    Done with the navigation menu. It looks okay, on mobile.

--- index.php
+++ index.php
@@ -1,42 +1,66 @@
-<?php 
-echo "
-<html>
-<html>
-	<head>
-		<link rel = 'stylesheet' type = 'text/css' href = 'mystyle.css' />
-		<title>Sanele Mpangalala</title>
-		<script src='https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js'></script>
-		<script>
-			
-			$(document).ready(function(){
-				var clicked=0;
-				$('.menu-icon').click(function(){
-					var leftVal = '10px';
-					
-					if(clicked===0){
-						$('.menu-icon2').animate({left:'250px'}, "slow");clicked=1;
-					}
-					else{$('.menu-icon2').animate({left:leftVal}, "slow");clicked=0;}
-					
+<?php
+	echo "
+		<html>
+		<head>
+			<title>Sanele Mpangalala</title>
+			<meta name='viewport' content='width=device-width, initial-scale=1.0'>
+			<link rel = 'stylesheet' type = 'text/css' href = 'mystyle.css'>
+			<script src='https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js'></script>
+			<script >
+				
+				$(document).ready(function(){
+					var clicked=0;
+					$('.menu-icon').click(function(){
+						var leftVal = '10px';
+						if(clicked===0){
+							$('.container').animate({left:'220px'}, 'slow');clicked=1;
+						}
+						else{
+							$('.container').animate({left:'5px'}, 'slow');clicked=0;
+						}
+						
+					});
 				});
-			});
-		</script>
-	</head>
+			</script>
+		</head>
 	<body>
+		<h1 id= 'title'>Sanele Mpangalala</h1>
+		
+		<!--div class='nav-menu'-->
+			<ul class = 'nav-menu'>
+				<li class = 'nav-item'><a href = 'index.php'>Home</a></li>
+				
+				<li class = 'nav-item drop-down'><a href = '#'>Projects</a>
+					<ul class='drop-down-content'>
+						<li><a href = '#'>CarbAnalyser</a></li>
+						<li><a href = '#'>Temperature Monitor</a></li>
+						<li><a href = '#'>Security System</a></li>
+						<li><a href = '#'>RESTful API</a></li>
+						<li><a href = '#'>Emailing Web App</a></li>
+					</ul>
+				</li>
+				<li class = 'nav-item'><a href = '#'>Blog</a></li>
+				<li class = 'nav-item'><a href = '#'>Contact</a></li>
+				<li class = 'nav-item'><a href = '#'>About</a></li>
+			</ul>
+		<!--Navigation Menu Icon for mobile-->
 		<div class = 'container'>
-			<div class = 'menu-icon2' >
-				<div class = 'bar-ting2'></div>
-				<div class = 'bar-ting2'></div>
-				<div class = 'bar-ting2'></div>
+			<div class = 'sliding-menu'>
+				<ul class = 'nav-menu-small'>
+					<li><a href= '#'>Home</a><li>
+					<li><a href= '#'>Projects</a><li>
+					<li><a href= '#'>Blog</a><li>
+					<li><a href= '#'>Contact</a><li>
+					<li><a href= '#'>About</a><li>
+				</ul>
 			</div>
-			<div class = 'menu-icon'  id = 'menu'>
-				<div class = 'bar-ting'></div>
-				<div class = 'bar-ting'></div>
-				<div class = 'bar-ting'></div>
+			<div class = 'menu-icon'>
+				<div class='bar-ting'></div>
+				<div class='bar-ting'></div>
+				<div class='bar-ting'></div>
 			</div>
 		</div>
-	<body>
-</html>
-";
-
+		<!--/div-->
+	</body>
+	</html>";
 ?>
